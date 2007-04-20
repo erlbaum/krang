924c2d155d80f94f0e99836179909fd71c336286
Michael Peters <mpeters@plusthree.com>
2007-04-20 19:56:21 +0000

    we need to explicitly set the values on the <option> elements or IE wont behave properly when we try and set the value using "input.value = 2" in Javascript
    git-svn-id: http://svn.krangcms.com/branches/ajax_and_new_skin@3783 689d98c0-9023-0410-9c0b-f79783857903

--- lib/Krang/Widget.pm
+++ lib/Krang/Widget.pm
@@ -424,30 +424,20 @@ sub time_chooser {
         <img alt="" src="images/clock.gif" id="${name}_trigger" class="clock_trigger">
         <div id="${name}_clock" class="clock_widget" style="display:none">
             <select name="${name}_hour" onchange="Krang.Widget.update_time_chooser('$name'); $onchange" disabled>
-                <option value="">Hour</option>
-                <option>1</option> <option>2</option>  <option>3</option>  <option>4</option>
-                <option>5</option> <option>6</option>  <option>7</option>  <option>8</option>
-                <option>9</option> <option>10</option> <option>11</option> <option>12</option>
+                <option value="">Hour</option> | 
+        . join(' ', map { qq|<option value="$_">$_</option>| } (1..12)) .
+        qq|
             </select>
             :
             <select name="${name}_minute" onchange="Krang.Widget.update_time_chooser('$name'); $onchange" disabled>
-                <option value="">Minute</option>
-                <option>00</option> <option>01</option> <option>02</option> <option>03</option> <option>04</option>
-                <option>05</option> <option>06</option> <option>07</option> <option>08</option> <option>09</option>
-                <option>10</option> <option>11</option> <option>12</option> <option>13</option> <option>14</option>
-                <option>15</option> <option>16</option> <option>17</option> <option>18</option> <option>19</option>
-                <option>20</option> <option>21</option> <option>22</option> <option>23</option> <option>24</option>
-                <option>25</option> <option>26</option> <option>27</option> <option>28</option> <option>29</option>
-                <option>30</option> <option>31</option> <option>32</option> <option>33</option> <option>34</option>
-                <option>35</option> <option>36</option> <option>37</option> <option>38</option> <option>39</option>
-                <option>40</option> <option>41</option> <option>42</option> <option>43</option> <option>44</option>
-                <option>45</option> <option>46</option> <option>47</option> <option>48</option> <option>49</option>
-                <option>50</option> <option>51</option> <option>52</option> <option>53</option> <option>54</option>
-                <option>55</option> <option>56</option> <option>57</option> <option>58</option> <option>59</option>
+                <option value="">Minute</option> |
+        . join(' ', map { qq|<option value="$_">$_</option>| } 
+            ('00', '01', '02', '03', '04', '05', '06', '07', '08', '09', 10..59)) .
+        qq|
             </select>
             &nbsp;
             <select name="${name}_ampm" onchange="Krang.Widget.update_time_chooser('$name'); $onchange" disabled>
-                <option>AM</option> <option>PM</option>
+                <option value="AM">AM</option> <option value="PM">PM</option>
             </select>
         </div>
         <script type="text/javascript">
