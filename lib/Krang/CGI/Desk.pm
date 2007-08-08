6259baaa39759a056d6b40829b22d33623df94c4
Michael Peters <mpeters@plusthree.com>
2007-08-08 20:14:53 +0000

    fixing bug where the story_id wasnt actually in the preview link
    git-svn-id: http://svn.krangcms.com/branches/ajax_and_new_skin@4017 689d98c0-9023-0410-9c0b-f79783857903

--- lib/Krang/CGI/Desk.pm
+++ lib/Krang/CGI/Desk.pm
@@ -142,7 +142,7 @@ sub _row_handler {
     $row->{story_type} = $obj->class->display_name;
     $row->{url} = format_url(url    => $obj->url,
                              linkto => 
-                             "javascript:Krang.preview('story',$row->{id})",
+                             "javascript:Krang.preview('story',$row->{story_id})",
                              length => 50);
     $row->{may_edit} = $obj->may_edit;
 
