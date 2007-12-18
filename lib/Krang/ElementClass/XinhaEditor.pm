1d2abac290f6aeb61e5d6c874726154ea9d7f2c9
Bodo Schulze <bobesch@letras.net>
2007-12-18 16:13:39 +0000

    Fixed XinhaEditor caching.
    git-svn-id: http://svn.krangcms.com/branches/krang_v3_01@4540 689d98c0-9023-0410-9c0b-f79783857903

--- lib/Krang/ElementClass/XinhaEditor.pm
+++ lib/Krang/ElementClass/XinhaEditor.pm
@@ -2,6 +2,8 @@ package Krang::ElementClass::XinhaEditor;
 use strict;
 use warnings;
 
+use Krang::ClassFactory qw(pkg);
+
 use base 'Krang::ElementClass::Textarea';
 use Carp qw(croak);
 
@@ -98,6 +100,8 @@ sub input_form {
     my ($param) = $self->param_names( element => $element );
     my $html = "";
 
+    my $install_id = pkg('Info')->install_id();
+
     # only add this once
     my @sibs = grep { $_->class->isa(__PACKAGE__) } 
       $element->parent()->children();
@@ -122,7 +126,7 @@ sub input_form {
     if (!Krang.XinhaLoaded) {
         var xinha_script = document.createElement('script');
         xinha_script.setAttribute('type', 'text/javascript');
-        xinha_script.setAttribute('src',  '/static/<tmpl_var krang_install_id>/xinha/htmlarea.js');
+        xinha_script.setAttribute('src',  '/static/$install_id/xinha/htmlarea.js');
         document.body.appendChild(xinha_script);
         Krang.XinhaLoaded = 1;
     }
