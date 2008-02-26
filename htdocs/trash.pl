21ce6bd0db681eeaf43da2571d987d7c83f82ba0
Bodo Schulze <bobesch@letras.net>
2008-02-26 12:08:32 +0000

    Posted Michaels heavy-armed gards in front of Krang::CGI::Trash's delete runmode.
    git-svn-id: http://svn.krangcms.com/branches/krang_v3_02@4886 689d98c0-9023-0410-9c0b-f79783857903

--- htdocs/trash.pl
+++ htdocs/trash.pl
@@ -3,10 +3,7 @@ use Krang::ClassFactory qw(pkg);
 use Krang::ClassLoader 'ErrorHandler';
 use Krang::ClassLoader 'CGI::Trash';
 pkg('CGI::Trash')->new(
-##    PARAMS => {
-# TODO
-##        PACKAGE_ASSETS => { story => [qw(read-only edit)] },
-##        RUNMODE_ASSETS => {
-##        },
-##    },
+    PARAMS => {
+        RUNMODE_PERMISSIONS => { delete_checked => [qw(admin_delete)] }
+    },
 )->run();
