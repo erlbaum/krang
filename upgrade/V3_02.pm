a66eae947d5190af38fa821545319abf58329838
Bodo Schulze <bobesch@letras.net>
2008-03-18 16:00:01 +0000

    Added trash table creation to V3_02 upgrade module.
    git-svn-id: http://svn.krangcms.com/branches/krang_v3_02@5016 689d98c0-9023-0410-9c0b-f79783857903

--- upgrade/V3_02.pm
+++ upgrade/V3_02.pm
@@ -30,6 +30,17 @@ sub per_instance {
     $dbh->do('Alter TABLE group_permission ADD COLUMN admin_delete BOOL NOT NULL DEFAULT 0');
     $dbh->do('Update group_permission SET admin_delete = 1 WHERE group_id = 1');
     $dbh->do('Update group_permission SET admin_delete = 1 WHERE group_id = 2');
+
+    # create the trashbin table
+    $dbh->do(<<SQL);
+CREATE TABLE trash (
+    object_type  varchar(255)      NOT NULL,
+    object_id 	 int(10) unsigned  NOT NULL,
+    timestamp    datetime          NOT NULL,
+    INDEX (object_type, object_id)
+) TYPE=MyISAM;
+SQL
+
 }
 
 1;
