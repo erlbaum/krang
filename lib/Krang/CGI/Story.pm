b2efa25abcf43a550300a0d465d5ba3653d73a2c
Michael Peters <mpeters@plusthree.com>
2008-05-02 18:23:50 +0000

    cant uswe @_ here cause it still contains $self. So use %args which is everything minus $self
    git-svn-id: http://svn.krangcms.com/branches/krang_v3_02@5112 689d98c0-9023-0410-9c0b-f79783857903

--- lib/Krang/CGI/Story.pm
+++ lib/Krang/CGI/Story.pm
@@ -1944,7 +1944,7 @@ sub _do_find {
             status          => 'Status',
         },
         columns_sortable => [qw( story_id title url cover_date )],
-        row_handler      => sub { $self->find_story_row_handler(@_, retired => $retired); },
+        row_handler      => sub { $self->find_story_row_handler(%args, retired => $retired); },
         id_handler => sub { return $_[0]->story_id },
     );
 
