5d80503f3deb478fb01ecf52393aad6f6622a03f
Noam Weinstein <nweinstein@erlbaum.net>
2008-11-26 00:03:44 +0000

    added code to v3.04 upgrade to fix ownership of newly-created media.pm module (and copied other changes from trunk, except for media->published flag which isn't yet available on this branch)
    git-svn-id: http://svn.krangcms.com/tags/krang_v3_04_erlbaum_dev_testing@5669 689d98c0-9023-0410-9c0b-f79783857903

--- upgrade/V3_04.pm
+++ upgrade/V3_04.pm
@@ -5,136 +5,144 @@ use Krang::ClassFactory qw(pkg);
 use Krang::ClassLoader base => 'Upgrade';
 use Krang::ClassLoader DB   => 'dbh';
 use Krang::ClassLoader 'ElementLibrary';
-use Krang::Conf qw(KrangRoot InstanceElementSet);
+use Krang::Conf qw(KrangUser KrangGroup KrangRoot InstanceElementSet);
 use File::Spec::Functions qw(catfile catdir);
 
 sub per_installation {
     my $self = shift;
-    
+
     print "Removing deprecated files... ";
-    
+
     # removed in v3.02:
     # svn diff http://svn.krangcms.com/tags/krang_v3_01_fc5 \
     #          http://svn.krangcms.com/tags/3.02 --summarize | grep '^D'
-    $self->remove_files( qw(
-        src/Imager-0.44.tar.gz
-        src/Image-Info-1.15.tar.gz
-        bin/smoke_test
-    ));
-    
+    $self->remove_files(
+        qw(
+          src/Imager-0.44.tar.gz
+          src/Image-Info-1.15.tar.gz
+          bin/smoke_test
+          )
+    );
+
     # removed in v3.03:
     # svn diff http://svn.krangcms.com/tags/3.02 \
     #          http://svn.krangcms.com/tags/3.03 --summarize | grep '^D'
-    $self->remove_files( qw(
-        src/JSON-1.07.tar.gz
-        src/CGI-Application-Plugin-JSON-0.3.tar.gz
-    ));
-        
+    $self->remove_files(
+        qw(
+          src/JSON-1.07.tar.gz
+          src/CGI-Application-Plugin-JSON-0.3.tar.gz
+          )
+    );
+
     # removed in v3.04:
     # svn diff http://svn.krangcms.com/tags/3.03 \
     #          http://svn.krangcms.com/trunk/krang --summarize | grep '^D'
-    $self->remove_files( qw(
-        htdocs/help/category.html
-        htdocs/help/contributor.html
-        htdocs/help/contributor_associate.html
-        htdocs/help/desk.html
-        htdocs/help/desk_admin.html
-        htdocs/help/group.html
-        htdocs/help/list_group.html
-        htdocs/help/log.html
-        htdocs/help/media_archived.html
-        htdocs/help/my_alerts.html
-        htdocs/help/my_pref.html
-        htdocs/help/schedule.html
-        htdocs/help/schedule_job.html
-        htdocs/help/site.html
-        htdocs/help/story_archived.html
-        htdocs/help/template_archived.html
-        htdocs/help/user.html
-        htdocs/help/workspace.html
-        src/Apache-MOD_PERL/mod_ssl-2.8.28-1.3.37.tar.gz
-        src/Apache-MOD_PERL/mm-1.4.0.tar.gz
-        src/Apache-MOD_PERL/apache_1.3.37.tar.gz
-        src/Digest-MD5-2.23.tar.gz
-        src/HTML-Parser-3.36.tar.gz
-        src/HTML-PopupTreeSelect-Dynamic-1.3.tar.gz
-        templates/footer.tmpl
-        templates/header.tmpl
-        templates/nav.tmpl
-        templates/About/about.tmpl
-        templates/Alert/message.tmpl
-        templates/Bugzilla/edit.tmpl
-        templates/Category/copy.tmpl
-        templates/Category/edit.tmpl
-        templates/Category/find.tmpl
-        templates/Category/new.tmpl
-        templates/Contrib/associate_list_view.tmpl
-        templates/Contrib/edit_view.tmpl
-        templates/Contrib/list_view.tmpl
-        templates/Desk/desk.tmpl
-        templates/DeskAdmin/edit.tmpl
-        templates/ElementEditor/edit.tmpl
-        templates/ElementEditor/find_media_link.tmpl
-        templates/ElementEditor/find_story_link.tmpl
-        templates/ElementEditor/view.tmpl
-        templates/Group/edit_categories.tmpl
-        templates/Group/edit_view.tmpl
-        templates/Group/list_view.tmpl
-        templates/HTMLPager/pager-internals.tmpl
-        templates/HTMLPager/pager-pagination.tmpl
-        templates/Help/help_footer.tmpl
-        templates/Help/help_header.tmpl
-        templates/History/show.tmpl
-        templates/ListGroup/edit.tmpl
-        templates/ListGroup/list_view.tmpl
-        templates/Login/forgot_pw.tmpl
-        templates/Login/forgot_pw_email.tmpl
-        templates/Login/login.tmpl
-        templates/Login/login_footer.tmpl
-        templates/Login/login_header.tmpl
-        templates/Login/reset_pw.tmpl
-        templates/Media/BulkUpload/choose.tmpl
-        templates/Media/edit_media.tmpl
-        templates/Media/list_active.tmpl
-        templates/Media/list_active_pager.tmpl
-        templates/Media/list_retired.tmpl
-        templates/Media/list_view.tmpl
-        templates/Media/list_view_pager.tmpl
-        templates/Media/transform_image.tmpl
-        templates/Media/view_media.tmpl
-        templates/MyAlerts/edit.tmpl
-        templates/MyPref/edit.tmpl
-        templates/Publisher/media_error.tmpl
-        templates/Publisher/progress.tmpl
-        templates/Publisher/publish_list.tmpl
-        templates/Schedule/edit.tmpl
-        templates/Schedule/edit_admin.tmpl
-        templates/Schedule/list_all.tmpl
-        templates/Site/edit.tmpl
-        templates/Site/list_view.tmpl
-        templates/Site/view.tmpl
-        templates/Story/edit.tmpl
-        templates/Story/find.tmpl
-        templates/Story/find_pager.tmpl
-        templates/Story/list_active.tmpl
-        templates/Story/list_active_pager.tmpl
-        templates/Story/list_retired.tmpl
-        templates/Story/new.tmpl
-        templates/Story/view.tmpl
-        templates/Template/edit.tmpl
-        templates/Template/list_active.tmpl
-        templates/Template/list_active_pager.tmpl
-        templates/Template/list_retired.tmpl
-        templates/Template/list_view.tmpl
-        templates/Template/list_view_pager.tmpl
-        templates/Template/view.tmpl
-        templates/Trash/trash.tmpl
-        templates/User/edit_view.tmpl
-        templates/User/list_view.tmpl
-        templates/Widget/category_chooser.tmpl
-        templates/Widget/template_chooser.tmpl
-        templates/Workspace/workspace.tmpl
-    ));
+    $self->remove_files(
+        qw(
+          htdocs/help/category.html
+          htdocs/help/contributor.html
+          htdocs/help/contributor_associate.html
+          htdocs/help/desk.html
+          htdocs/help/desk_admin.html
+          htdocs/help/group.html
+          htdocs/help/list_group.html
+          htdocs/help/log.html
+          htdocs/help/media_archived.html
+          htdocs/help/my_alerts.html
+          htdocs/help/my_pref.html
+          htdocs/help/schedule.html
+          htdocs/help/schedule_job.html
+          htdocs/help/site.html
+          htdocs/help/story_archived.html
+          htdocs/help/template_archived.html
+          htdocs/help/user.html
+          htdocs/help/workspace.html
+          src/Apache-MOD_PERL/mod_ssl-2.8.28-1.3.37.tar.gz
+          src/Apache-MOD_PERL/mm-1.4.0.tar.gz
+          src/Apache-MOD_PERL/apache_1.3.37.tar.gz
+          src/Digest-MD5-2.23.tar.gz
+          src/Pod-Simple-0.96.tar.gz
+          src/HTML-Parser-3.36.tar.gz
+          src/HTML-PopupTreeSelect-Dynamic-1.3.tar.gz
+          src/WWW-Mechanize-1.10.tar.gz
+          templates/footer.tmpl
+          templates/header.tmpl
+          templates/nav.tmpl
+          templates/About/about.tmpl
+          templates/Alert/message.tmpl
+          templates/Bugzilla/edit.tmpl
+          templates/Category/copy.tmpl
+          templates/Category/edit.tmpl
+          templates/Category/find.tmpl
+          templates/Category/new.tmpl
+          templates/Contrib/associate_list_view.tmpl
+          templates/Contrib/edit_view.tmpl
+          templates/Contrib/list_view.tmpl
+          templates/Desk/desk.tmpl
+          templates/DeskAdmin/edit.tmpl
+          templates/ElementEditor/edit.tmpl
+          templates/ElementEditor/find_media_link.tmpl
+          templates/ElementEditor/find_story_link.tmpl
+          templates/ElementEditor/view.tmpl
+          templates/Group/edit_categories.tmpl
+          templates/Group/edit_view.tmpl
+          templates/Group/list_view.tmpl
+          templates/HTMLPager/pager-internals.tmpl
+          templates/HTMLPager/pager-pagination.tmpl
+          templates/Help/help_footer.tmpl
+          templates/Help/help_header.tmpl
+          templates/History/show.tmpl
+          templates/ListGroup/edit.tmpl
+          templates/ListGroup/list_view.tmpl
+          templates/Login/forgot_pw.tmpl
+          templates/Login/forgot_pw_email.tmpl
+          templates/Login/login.tmpl
+          templates/Login/login_footer.tmpl
+          templates/Login/login_header.tmpl
+          templates/Login/reset_pw.tmpl
+          templates/Media/BulkUpload/choose.tmpl
+          templates/Media/edit_media.tmpl
+          templates/Media/list_active.tmpl
+          templates/Media/list_active_pager.tmpl
+          templates/Media/list_retired.tmpl
+          templates/Media/list_view.tmpl
+          templates/Media/list_view_pager.tmpl
+          templates/Media/transform_image.tmpl
+          templates/Media/view_media.tmpl
+          templates/MyAlerts/edit.tmpl
+          templates/MyPref/edit.tmpl
+          templates/Publisher/media_error.tmpl
+          templates/Publisher/progress.tmpl
+          templates/Publisher/publish_list.tmpl
+          templates/Schedule/edit.tmpl
+          templates/Schedule/edit_admin.tmpl
+          templates/Schedule/list_all.tmpl
+          templates/Site/edit.tmpl
+          templates/Site/list_view.tmpl
+          templates/Site/view.tmpl
+          templates/Story/edit.tmpl
+          templates/Story/find.tmpl
+          templates/Story/find_pager.tmpl
+          templates/Story/list_active.tmpl
+          templates/Story/list_active_pager.tmpl
+          templates/Story/list_retired.tmpl
+          templates/Story/new.tmpl
+          templates/Story/view.tmpl
+          templates/Template/edit.tmpl
+          templates/Template/list_active.tmpl
+          templates/Template/list_active_pager.tmpl
+          templates/Template/list_retired.tmpl
+          templates/Template/list_view.tmpl
+          templates/Template/list_view_pager.tmpl
+          templates/Template/view.tmpl
+          templates/Trash/trash.tmpl
+          templates/User/edit_view.tmpl
+          templates/User/list_view.tmpl
+          templates/Widget/category_chooser.tmpl
+          templates/Widget/template_chooser.tmpl
+          templates/Workspace/workspace.tmpl
+          )
+    );
 }
 
 sub per_instance {
@@ -200,10 +208,11 @@ sub per_instance {
 
     # 5. add element support to media objects
     $self->add_elements_to_media();
+
 }
 
 sub add_elements_to_media {
-    my ($element_lib, $warn_about_new_pm, $warn_about_set_conf);
+    my ($element_lib, $warn_about_new_pm, $warn_about_set_conf, $warn_about_chown_failure);
 
     # 1. add column to media table
     my $dbh = dbh();
@@ -307,6 +316,13 @@ sub new {
 EOF
         print "done\n\n";
         $warn_about_new_pm = 1;
+        eval { 
+             my ($uid, $gid);
+             (undef, undef, $uid, undef) = getpwnam(KrangUser);
+             (undef, undef, $gid, undef) = getgrnam(KrangGroup);
+             chown($uid, $gid, $path_to_module);
+        };
+        $warn_about_chown_failure = 1 if $@;
 
         # 6. add media element to set.conf
         print "Adding an entry to $element_lib/set.conf... ";
@@ -366,6 +382,10 @@ EOF
     if ($warn_about_new_pm) {
         print
           "\n* * * WARNING * * * \t The Krang upgrade script has created a new file: $element_lib/$class.pm\n";
+    }
+    if ($warn_about_chown_failure) {
+        print
+          "\n* * * WARNING * * * \t The Krang upgrade script was unable to correct the ownership of $element_lib/$class.pm\n";
     }
     if ($warn_about_set_conf) {
         print
