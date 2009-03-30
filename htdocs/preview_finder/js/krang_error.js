3f24d16d967fdb7815e217e69f6633dd9906c6aa
Bodo Schulze <bobesch@letras.net>
2009-03-30 21:52:23 +0000

    Added Krang.Messages and Krang.Error library files.
    git-svn-id: http://svn.krangcms.com/branches/preview_editor@6042 689d98c0-9023-0410-9c0b-f79783857903

---
+++ htdocs/preview_finder/js/krang_error.js
@@ -0,0 +1,25 @@
+if (Krang == undefined) { Krang = {} }
+
+/**
+   Krang.Error
+*/
+Krang.Error = function(baseURL, error) {
+    // default error
+    if (!error) {
+        error = 'Looks like a little bug (probably an Internal Server Error)<br/>Contact your System Administrator if this problem continues.';
+    }
+
+    // default base URL
+    if (!baseURL) {
+        baseURL = '';
+    }
+
+    ProtoPopup.Alert.get('krang_preview_editor_error', {
+        modal:  true,
+        width:  '500px',
+        body:   error,
+        cancelIconSrc:           false,
+        bodyBackgroundImage:     'url("' + baseURL + '/images/bug.gif")',
+        closeBtnBackgroundImage: 'url("' + baseURL + '/images/bkg-button-mini.gif")'
+    }).show();
+}
