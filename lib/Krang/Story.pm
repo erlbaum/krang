387448ed8551d62dcbfd3a3bfca158131696003e
Noam Weinstein <nweinstein@erlbaum.net>
2009-08-03 20:47:30 +0000

    fixed uninitialized warning thrown by Story.pm during test suite
    git-svn-id: http://svn.krangcms.com/tags/V3_05@6209 689d98c0-9023-0410-9c0b-f79783857903

--- lib/Krang/Story.pm
+++ lib/Krang/Story.pm
@@ -312,7 +312,7 @@ sub category {
     # root category
     if ($arg{depth_only}) {
         my $ret = scalar($self->{category_cache}[0]->ancestors());
-        return ($ret == undef) ? 0 : $ret;
+        return (defined($ret) ? ret : 0);
     }
 
     # unrecognized arguments
